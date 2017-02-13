ee2be3fe991e99c0545f1932646e06ea49fbd725
paraboli <heather.michelle114@gmail.com>
2017-02-13 10:12:47 -0500

    added files and folders

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*Javascript*/
+
+$(document).ready(main);
+
+function main() {
+  $('h1').hide();
+  $('h1').fadeIn(2000);
+  $('h2').hide();
+  $('h2').fadeIn(3000);
+  $('h3').hide();
+  $('h3').fadeIn(4000);
+}
